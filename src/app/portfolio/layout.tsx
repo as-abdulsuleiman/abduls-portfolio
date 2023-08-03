7173f4a49fa256287d81ad07fccfc1a032a3e230
Abdul Suleiman <abdulrazaq.suleiman@pramie.tech>
2023-08-03 18:46:04 +0530

    minor chamges

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -5,6 +5,74 @@ import SideBar from "@/components/sidebar";
 import Tabs from "@/components/tabs";
 import ScrollProvider from "@/scroll-provider";
 import Navbar from "@/components/navbar";
+import {
+  openGraphImage,
+  keywords,
+  appleIcons,
+  iconShortcut,
+  archiveItems,
+} from "../shared-metadata";
+
+export const metadata = {
+  title: "Abdul Suleiman / Software Developer",
+  description: "Software developer at pramie.tech",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  openGraph: {
+    ...openGraphImage,
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@Abdul__Suleiman",
+    creator: "@Abdul__Suleiman",
+    title: "Abdul's Portfolio",
+    description: "Software developer at pramie.tech",
+    images: [`/card-bg.png`],
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: [...iconShortcut],
+    apple: [...appleIcons],
+    other: {
+      rel: "apple-icon-precomposed",
+      url: "/favicon/apple-icon-precomposed.png",
+    },
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  referrer: "origin-when-cross-origin",
+  keywords: [...keywords],
+  manifest: "/favicon/manifest.json",
+  archives: [...archiveItems],
+  category: "technology",
+  themeColor: [
+    { media: "(prefers-color-scheme: dark)", color: "#262626" },
+    { media: "(prefers-color-scheme: light)", color: "#262626" },
+  ],
+  applicationName: "Abdul's Portfolio",
+  authors: [
+    {
+      name: "Abdul Suleiman",
+      url: "https://www.linkedin.com/in/abdul-suleiman-9448021b7/",
+    },
+  ],
+  metadataBase: new URL(`${process.env.NEXT_PUBLIC_WEBSITE_URL}`),
+  alternates: {
+    canonical: "/",
+  },
+};
 
 export default function PortfolioLayout({
   children,
