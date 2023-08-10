827eaed6e81e1d213d541c189889d0171557c9a3
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2023-08-11 02:13:36 +0530

    chore: update website content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,9 @@ import {
   archiveItems,
 } from "./shared-metadata";
 
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
