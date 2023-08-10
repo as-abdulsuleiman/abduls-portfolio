e903c3a48a4c4ce47bac514ce658bb5cf4e513c5
Abdul Suleiman <abdulrazaq.suleiman@pramie.tech>
2023-08-10 19:40:05 +0530

    chore: update website content

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -17,7 +17,7 @@ export default function PortfolioLayout({
       <div className="relative py-[2rem] container full px-[20px] lg:px-[2rem] max-w-6xl mx-auto scroll-py-9 sm:scroll-py-16 snap-y snap-start">
         {projectModal}
         <div className="flex flex-col w-full lg:flex-row">
-          <div className="mx-auto lg:ml-0 w-full hidden lg:flex lg:w-[350px] relative lg:fixed z-40 flex-col border-[#717070] border-[0.1px] px-6 py-7 rounded-xl drop-shadow-md shadow-lg ">
+          <div className="mx-auto lg:ml-0 w-full hidden lg:flex lg:w-[350px] relative lg:fixed z-40 flex-col border-[#717070] border-[0.1px] px-6 py-7 rounded-xl drop-shadow-md shadow-lg">
             <SideBar />
           </div>
           <main className="ml-0 lg:ml-[300px] w-full h-full">
