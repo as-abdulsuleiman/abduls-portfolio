6f6cbabb1c3dc0aab67485437cdc0885e80dc87c
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2023-08-17 23:33:59 +0530

    chore: update projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,8 +21,8 @@ export const metadata = {
   viewport: {
     width: "device-width",
     initialScale: 1,
-    maximumScale: 1,
-    minimumScale: 0.1,
+    maximumScale: 5,
+    minimumScale: 1,
     userScalable: 1,
   },
   twitter: {
