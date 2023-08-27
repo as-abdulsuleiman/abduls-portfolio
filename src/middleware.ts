c4ef99f375c9acfdaab9dc9d059fe98ed702317f
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2023-08-27 22:25:25 +0530

    chore: minor changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,6 @@ import type { NextRequest } from 'next/server'
  
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/") {
-    return NextResponse.rewrite(new URL('/portfolio', request.url))
+    return NextResponse.redirect(new URL('/portfolio', request.url))
   }
 }
